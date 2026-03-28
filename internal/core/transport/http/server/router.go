c4907559be5bbdd3e70a044f20cc18af64942852
Adopten123 <alexandermikhailov0505@gmail.com>
2026-03-28 13:44:21 +0300

    router

--- internal/core/transport/http/server/router.go
+++ internal/core/transport/http/server/router.go
@@ -1,4 +1,16 @@
 package core_http_server
 
+import "net/http"
+
+type ApiVersion string
+
+var (
+	ApiVersion1 ApiVersion = "v1"
+	ApiVersion2 ApiVersion = "v2"
+	ApiVersion3 ApiVersion = "v3"
+)
+
 type APIVersionRouter struct {
+	*http.ServeMux
+	apiVersion ApiVersion
 }
